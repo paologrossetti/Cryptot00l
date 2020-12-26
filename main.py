69c08304cc148d63bd9365147f8a80f0d4f0011a
Paolo <grossetti.paolo@libero.it>
2020-12-26 23:38:18 +0100

    Message Encryption with RSA

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from CryptoClass.AESClass import AESClass
+from CryptoClass.RSAClass import RSAClass
 
 if __name__ == '__main__':
-    aes = AESClass("this are 16bytes", "OFB")
+    '''aes = AESClass("this are 16bytes", "OFB")
     res_enc = aes.encrypt_file("/tmp/test_AES")#res_enc = aes.encrypt_string("secret")
     if res_enc:
-        aes.decrypt_file(res_enc)#aes.decrypt_string(res_enc)
+        aes.decrypt_file(res_enc)#aes.decrypt_string(res_enc)'''
+    rsa_alice = RSAClass('alice')
+    rsa_bob = RSAClass('bob')
+    data = rsa_alice.send_message('bob', 'Eve does not exists!')
+    rsa_bob.receive_message(data)
+
